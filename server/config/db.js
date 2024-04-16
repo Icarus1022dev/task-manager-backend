05fbe050cc4948ce8c176079c44648403442aeaa
cryptoDev <icarus.senior.dev@gmail.com>
2024-04-16 02:02:18 -0700

    completed

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+const mongoURI = 'mongodb+srv://root:QWERqwer1234@cluster0.cagnj.mongodb.net/auction_raffle_evm?retryWrites=true&w=majority';
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+module.exports = {mongoURI, options}
